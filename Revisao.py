ec5d8a399c9860b1cdac7633851521a0a4c14cbf
Humberto-Matondo <Humbertomat99@gmail.com>
2026-01-05 10:23:34 +0100

    arrays_1

--- Revisao.py
+++ Revisao.py
@@ -1,10 +1,10 @@
-frase = 'Humberto Matondo'
+from array import array
+
+balances = [100, 200, 300, 400, 500]
+
+for i in range(len(balances)):
+    balances[i] *= 2
 
 
-print(frase[0]) 
-print(frase[15])
-print(frase[9:16])
-print(frase[9:16:2])
-print(frase[:8])
-print(frase[9:])
-print(frase[9::3]) 
+for i in range(len(balances)):
+    print(balances[i])
