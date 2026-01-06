1f35b1722700b704dc86f6343b788d9fd146ff0f
Humberto-Matondo <Humbertomat99@gmail.com>
2026-01-06 20:23:36 +0100

    classes_3

--- Classes.py
+++ Classes.py
@@ -1,9 +1,10 @@
 class Jogador:
-    def __init__(self, nome, posicao, numero, pontos):
+    def __init__(self, nome:str, posicao:str, numero:int, pontos:int, vida:int):
         self.nome = nome
         self.posicao = posicao
         self.numero = numero
         self.pontos = pontos
+        self.vida = vida
 
     def info(self):
         print(f"Nome: {self.nome} \nPosição: {self.posicao} \nNúmero: {self.numero} \nPontos: {self.pontos}\n")
@@ -12,13 +13,18 @@ class Jogador:
         self.pontos += int(self.pontos * 0.50)
         print(f"{self.nome} agora tem {self.pontos} pontos!\n")
 
-jogador1 = Jogador("Ronaldo", "Atacante", 7, 1)
-jogador2 = Jogador("Messi", "Atacante", 10, 2)
-jogador3 = Jogador("Mbappe", "Atacante", 9, 3 )
+    def atacar (self, alvo):
+        alvo.vida -= self.pontos
+        print(f"{self.nome} está atacando {alvo.nome} que ficou com {alvo.vida} de pontos de vida!\n")
+
+jogador1 = Jogador("Ronaldo", "Atacante", 7, 1, 10)
+jogador2 = Jogador("Messi", "Atacante", 10, 2, 10)
+jogador3 = Jogador("Mbappe", "Atacante", 9, 3, 10)
 
 jogador1.info()
 jogador2.info()
 jogador3.info()
 
 jogador3.powerup()
+jogador2.atacar(jogador1)
 
