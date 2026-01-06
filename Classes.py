9cd29d395985e41bf752291910bd9ee4388700fd
Humberto-Matondo <Humbertomat99@gmail.com>
2026-01-06 20:16:52 +0100

    classes_2

--- Classes.py
+++ Classes.py
@@ -1,11 +1,24 @@
 class Jogador:
-    def __init__(self, nome, posicao, numero):
+    def __init__(self, nome, posicao, numero, pontos):
         self.nome = nome
         self.posicao = posicao
         self.numero = numero
+        self.pontos = pontos
 
     def info(self):
-        print(f"Nome: {self.nome} \nPosição: {self.posicao} \nNúmero: {self.numero}")
+        print(f"Nome: {self.nome} \nPosição: {self.posicao} \nNúmero: {self.numero} \nPontos: {self.pontos}\n")
 
-jogador1 = Jogador("Ronaldo", "Atacante", 9)
+    def powerup(self):
+        self.pontos += int(self.pontos * 0.50)
+        print(f"{self.nome} agora tem {self.pontos} pontos!\n")
+
+jogador1 = Jogador("Ronaldo", "Atacante", 7, 1)
+jogador2 = Jogador("Messi", "Atacante", 10, 2)
+jogador3 = Jogador("Mbappe", "Atacante", 9, 3 )
+
 jogador1.info()
+jogador2.info()
+jogador3.info()
+
+jogador3.powerup()
+
